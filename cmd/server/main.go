75ba912a77d20518428f3de4f47d48046c69969d
Juan Esteban Lopez <j.lopezt@uniandes.edu.co>
2026-04-11 22:56:14 -0500

    feat: initial project structure and module setup

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Cloud Log Analyzer: Ingestor iniciado...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/juaneslot01/cloud-log-analyzer
+
+go 1.26.2
